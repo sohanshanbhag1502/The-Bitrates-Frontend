2d17044eaa961ac9fd52363074bc680611ed9d2f
nik-216 <nikitha.roj@gmail.com>
2025-12-01 05:11:01 +0000

    Integrated socket and minor modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
 import VideoSyncPage from "./pages/VideoSyncPage";
-import SignupPage from "./pages/SignupPage";   // ← add this
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<VideoSyncPage />} />
-        <Route path="/videosync" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />  {/* ← add route */}
       </Routes>
     </BrowserRouter>
   );
